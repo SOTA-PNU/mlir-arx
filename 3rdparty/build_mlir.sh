f4248ee418d1f48d517c5935ab96edc7ae6c9554
m0z3li <limoze0327@swu.ac.kr>
2025-02-03 07:44:05 +0000

    Add new file

---
+++ 3rdparty/build_mlir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# pwd: /home/user/app
+# chmod 655 build_mlir.sh
+git clone -n https://github.com/llvm/llvm-project.git
+cd llvm-project && git checkout e86910337f98e57f5b9253f7d80d5b916eb1d97e && cd ..
+
+mkdir llvm-project/build
+cd llvm-project/build
+
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS="mlir;clang;openmp" \
+   -DLLVM_TARGETS_TO_BUILD="host" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DLLVM_ENABLE_RTTI=ON \
+   -DENABLE_LIBOMPTARGET=OFF \
+   -DLLVM_ENABLE_LIBEDIT=OFF
+
+cmake --build . -- ${MAKEFLAGS}
+cmake --build . --target check-mlir
