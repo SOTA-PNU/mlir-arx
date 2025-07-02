fa86e044642da422172cf0ea92c762a5fba68e49
JooHyoung <chacha@udon.party>
2025-07-02 08:38:24 -0400

    add: HARX dialect

---
+++ src/Accelerators/ARX/Dialect/HARX/HARXOps/Elementwise/Elementwise.cpp
@@ -0,0 +1,70 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===------------------ Elementwise.cpp - HARX Operations ----------------===//
+//
+// Copyright 2019-2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/Accelerators/ARX/Dialect/HARX/HARXOps/ShapeHelper.hpp"
+
+using namespace mlir;
+using namespace onnx_mlir;
+
+namespace onnx_mlir {
+namespace harx {
+
+//===----------------------------------------------------------------------===//
+// AddOp
+LogicalResult HARXAddOp::inferShapes(
+    std::function<void(Region &)> doShapeInference) {
+  return inferShapeForUnaryOps(this->getOperation());
+}
+
+//===----------------------------------------------------------------------===//
+// SubOp
+
+LogicalResult HARXSubOp::inferShapes(
+    std::function<void(Region &)> doShapeInference) {
+  return inferShapeForUnaryOps(this->getOperation());
+}
+
+//===----------------------------------------------------------------------===//
+// MulOp
+
+LogicalResult HARXMulOp::inferShapes(
+    std::function<void(Region &)> doShapeInference) {
+  return inferShapeForUnaryOps(this->getOperation());
+}
+
+//===----------------------------------------------------------------------===//
+// DivOp
+
+LogicalResult HARXDivOp::inferShapes(
+    std::function<void(Region &)> doShapeInference) {
+  return inferShapeForUnaryOps(this->getOperation());
+}
+
+//===----------------------------------------------------------------------===//
+// ReluOp
+
+LogicalResult HARXReluOp::inferShapes(
+    std::function<void(Region &)> doShapeInference) {
+  return inferShapeForUnaryOps(this->getOperation());
+}
+
+//===----------------------------------------------------------------------===//
+// TanhOp
+
+LogicalResult HARXTanhOp::inferShapes(
+    std::function<void(Region &)> doShapeInference) {
+  return inferShapeForUnaryOps(this->getOperation());
+}
+
+} // namespace harx
+} // namespace onnx_mlir
