ba00dc728b675473318638451fb324d1b7d0d078
JooHyoung Cha <chacha@udon.party>
2025-08-12 01:35:25 +0900

    implemented mlir-arx and add unit test code

--- src/Accelerators/ARX/Compiler/ARXCompilerOptions.cpp
+++ src/Accelerators/ARX/Compiler/ARXCompilerOptions.cpp
@@ -16,10 +16,14 @@
 #define DEBUG_TYPE "ARXCompilerOptions"
 
 namespace onnx_mlir {
-
-llvm::cl::opt<bool> arxEnableCuteChaCha("arx-cute-chacha",
-    llvm::cl::desc("HAHA 나는 매우 귀엽지!"),
-    llvm::cl::init(false), llvm::cl::cat(OnnxMlirOptions));
-
+   
+    llvm::cl::opt<ARXEmissionTargetType> arxEmissionTarget(
+        llvm::cl::desc("[Optional] Choose ARX-related target to emit "
+                       "(once selected it will cancel the other targets):"),
+        llvm::cl::values(
+            clEnumVal(EmitARXIR, "Lower model to ARX IR (ARX dialect)"),
+            clEnumVal(EmitEmitCIR, "Lower model to EmitCIR IR (EmitCIR dialect)"),
+            clEnumVal(EmitNONE, "Do not emit ARX-related target (default)")),
+        llvm::cl::init(EmitNONE), llvm::cl::cat(OnnxMlirOptions));
     
 } // namespace onnx_mlir
